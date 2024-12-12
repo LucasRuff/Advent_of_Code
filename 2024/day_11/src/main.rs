db6fdc1769b2cd2f706549a831ab21727aaaa647
LucasRuff <l.c.ruff@gmail.com>
2024-12-12 09:05:01 -0500

    made a day 11 part 2 that doesn't work

--- 2024/day_11/src/main.rs
+++ 2024/day_11/src/main.rs
@@ -8,12 +8,22 @@ fn main() {
     let now = Instant::now();
     if let Ok(mut file_iter) = read_lines("input_11.txt") {
         let txt = file_iter.next().unwrap().unwrap();
-        let mut total = 0;
-        for stone_txt in txt.split(" ") {
+        let mut total1 = 0;
+        let mut total2 = 0;
+        let mut blink_memo = HashMap::new();
+        for stone_txt in txt.clone().split(" ") {
             let stone = stone_txt.parse::<u128>().unwrap();
-            total += blink_stone_short(stone, 0);
+            total1 += blink_stone_short(stone, 0);
         }
-        println!("total 1: {}", total);
+        println!("total 1: {}", total1);
+        println!("Finished in {:?}", now.elapsed());
+        for stone_txt in txt.clone().split(" ") {
+            let stone = stone_txt.parse::<u128>().unwrap();
+            let (res_total, res_memo) = blink_stone_long(stone, 75, blink_memo);
+            blink_memo = res_memo;
+            total2 += res_total;
+        }
+        println!("total 2: {}", total2);
     }
     println!("Finished in {:?}", now.elapsed());
 }
@@ -26,42 +36,84 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn blink_stone_long(stone: u128, depth: usize, max_depth: usize, answer_memo: &HashMap<(u128, usize), usize>) -> (usize, HashMap<(u128, usize), usize>) {
-    if depth == max_depth {
-        let mut tmp_memo = answer_memo.clone();
-        tmp_memo.insert((stone, depth), 1);
-        return (1, tmp_memo);
-    } else {
-        let mut tmp_memo = answer_memo.clone();
-        if stone == 0 {
-            let poss = match answer_memo.get(&(1, depth + 1)) {
-                Some(b) => b.to_owned(),
-                None => blink_stone_long(1, depth + 1, max_depth, answer_memo).0,
-            };
-            tmp_memo.insert((stone, depth), poss);
-            return (poss, tmp_memo);
+fn blink_stone_tree() {}
+
+/*
+fn blink_stone_long(parent_stone: u128, max_depth: usize, start_memo: HashMap<u128, (usize, (u128, u128))>) -> (usize, HashMap<u128, (usize, (u128, u128))>) {
+    let mut memo = start_memo.clone();
+    let mut frontier = vec![Stone{val: parent_stone, depth: 0}];
+    let mut total = 0;
+    'bigloop: loop {
+        let current_stone = frontier.pop();
+        match current_stone {
+            Some(s) => {
+                match memo.get(&s.val) {
+                    Some((steps, (stone_1_val, stone_2_val))) => {
+                        let moving_depth = s.depth + steps;
+                        if moving_depth <= max_depth {
+                            let (stone_1, stone_2) = (Stone{val: *stone_1_val, depth: moving_depth},
+                            Stone{val: *stone_2_val, depth: moving_depth});
+                            frontier.push(stone_1);
+                            frontier.push(stone_2);
+                            continue 'bigloop;
+                        } else {
+                            total += 1;
+                            continue 'bigloop;
+                        }
+                    }
+                    None => {
+                        println!("memo miss on {}", s.val);
+                    },
+                }
+                if s.depth == max_depth {
+                    total += 1;
+                    continue 'bigloop;
+                }
+                let mut num_digits: u32 = if s.val != 0 {(s.val.ilog10() + 1).try_into().unwrap()} else {1};
+                let mut moving_val = s.val;
+                let mut moving_depth = s.depth;
+                'small_loop: loop {
+                    
+                    match (num_digits % 2 == 0, moving_val == 0) {
+                        (false,true) => {
+                            moving_val = 1;
+                            moving_depth += 1;
+                            
+                        },
+                        (true,_) => {
+                            moving_depth += 1;
+                            
+                            let (stone_1, stone_2) = (Stone{val: moving_val / 10_u128.pow(num_digits / 2), depth: moving_depth},
+                            Stone{val: moving_val % (10_u128.pow(num_digits / 2)), depth: moving_depth});
+                            memo.insert(s.val, (moving_depth - s.depth, (stone_1.val, stone_2.val)));
+                            println!("New memo entry for {}", moving_val);
+                            if moving_depth == max_depth {
+                                total += 2;
+                                continue 'bigloop;
+                            }
+                            frontier.push(stone_1);
+                            frontier.push(stone_2);
+                            continue 'bigloop;
+
+                        },
+                        (false,false) => {
+                            moving_val = moving_val * 2024;
+                            moving_depth += 1;
+                        },
+                    }
+                    if moving_depth == max_depth {
+                        total += 1;
+                        continue 'bigloop;
+                    }
+                    num_digits = if moving_val != 0 {(moving_val.ilog10() + 1).try_into().unwrap()} else {1};
+                }
+            },
+            None => break 'bigloop,
         }
-        let num_digits: u32 = (stone.ilog10() + 1).try_into().unwrap();
-        if num_digits % 2 == 0 {
-            let poss_1 = match answer_memo.get(&(stone / (10_u128.pow(num_digits / 2)), depth + 1)) {
-                Some(a) => a.to_owned(),
-                None => blink_stone_long(stone / (10_u128.pow(num_digits / 2)), depth + 1, max_depth, answer_memo).0,
-            };
-            let poss_2 = match answer_memo.get(&(stone % (10_u128.pow(num_digits / 2)), depth + 1)) {
-                Some(b) => b.to_owned(),
-                None => blink_stone_long(stone % (10_u128.pow(num_digits / 2)), depth + 1, max_depth, answer_memo).0,
-            };
-            tmp_memo.insert((stone, depth), poss_1 + poss_2);
-            return (poss_1 + poss_2, tmp_memo);
-        }
-        let poss = match answer_memo.get(&(stone * 2024, depth + 1)) {
-            Some(a) => a.to_owned(),
-            None => blink_stone_long(stone * 2024, depth + 1, max_depth, answer_memo).0,
-        };
-        tmp_memo.insert((stone, depth), poss);
-        return (poss, tmp_memo);
     }
+    return (total, memo);
 }
+*/
 
 fn blink_stone_short(stone: u128, depth: usize) -> usize {
     if depth == 25 {
@@ -76,6 +128,12 @@ fn blink_stone_short(stone: u128, depth: usize) -> usize {
         }
         return blink_stone_short(stone * 2024, depth + 1);
     }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Stone {
+    val: u128,
+    depth: usize,
 }
 
 #[cfg(test)]
@@ -102,7 +160,8 @@ mod tests {
             let mut answer_memo = HashMap::new();
             for stone_txt in txt.split(" ") {
                 let stone = stone_txt.parse::<u128>().unwrap();
-                let (partial_total, answer_memo) = blink_stone_long(stone, 0, 75, &answer_memo);
+                let (partial_total, temp_memo) = blink_stone_long(stone, 25, answer_memo);
+                answer_memo = temp_memo;
                 total += partial_total;
             }
             assert_eq!(total, 0);
